defeab1ee9133fc92ce7c75b158eed8db5af5f82
Pi-face <guzdev52@gmail.com>
2025-01-25 14:17:08 -0600

    Bubble Shooting

--- Assets/Scripts/Gameplay/PlayerShooting.cs
+++ Assets/Scripts/Gameplay/PlayerShooting.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    public GameObject bubblePrefab;
     public Transform firePoint;
-    public float bulletSpeed = 10f;
+    public float bubbleSpeed = 3f;
+    public float upwardForce = 2f;
 
     void Update()
     {
@@ -17,18 +19,24 @@ public class PlayerShooting : MonoBehaviour
 
     void Shoot()
     {
-       // initialize the projectile on player.
-       GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
-       
-       // Determine shooting direction.
-       Vector2 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
-       
-       // set velocity 
-       Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-       if (rb != null)
-       {
-           rb.velocity = direction * bulletSpeed;
-       }
-       
+        //Spawn bubble.
+        GameObject bubble = Instantiate(bubblePrefab, firePoint.position, Quaternion.identity);
+        
+        //Determine the direction based on the players direction.
+        Vector2 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+        
+        // Add a slightly upward movement to the velocity.
+        Vector2 bubbleVelocity = (direction * bubbleSpeed) + (Vector2.up * upwardForce);
+        
+        // Add randomness to make it feel more natural.
+        bubbleVelocity += new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(0, 0.5f));
+        
+        // Apply the velocity to the RigidBody2D
+        Rigidbody2D rb = bubble.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = bubbleVelocity;
+        }
     }
 }
