ca453fe8cf6d5ba44dbf7d0585b451846045d6bd
ben <benjaminconnick@gmail.com>
2025-01-26 11:53:53 -0600

    vcam optional

--- Assets/Scripts/Mechanics/LevelSystem.cs
+++ Assets/Scripts/Mechanics/LevelSystem.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
+using Cinemachine;
 using Platformer.Core;
 using Platformer.Gameplay;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace Platformer.Mechanics
 {
@@ -22,9 +24,21 @@ namespace Platformer.Mechanics
             int nextLevelSceneIndex = CurrentLevel;
             var asyncOp = SceneManager.LoadSceneAsync(nextLevelSceneIndex, LoadSceneMode.Additive);
             yield return new WaitUntil(() => asyncOp == null || asyncOp.isDone);
-            Simulation.Schedule<PlayerSpawn>();
+            DoLevelSetup();
             int prevLevelSceneIndex = CurrentLevel-1; // zero indexed
             SceneManager.UnloadSceneAsync(prevLevelSceneIndex);
+        }
+
+        private void DoLevelSetup()
+        {
+            Simulation.Schedule<PlayerSpawn>();
+
+            var vCam = Object.FindObjectOfType<CinemachineVirtualCamera>();
+            if (vCam != null)
+            {
+                vCam.m_Follow = GameController.Instance.model.player.transform;
+                vCam.m_LookAt = GameController.Instance.model.player.transform;
+            }
         }
     }
 }
